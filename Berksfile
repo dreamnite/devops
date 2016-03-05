59d0d86a01814e63446c68466e29c352b4c1f657
Mike Morrow <mmorrow@vt.edu>
2016-03-05 18:53:13 -0500

    Initial commit

---
+++ Berksfile
@@ -0,0 +1,4 @@
+source "https://supermarket.chef.io"
+cookbook "include_me", :git => "https://github.com/dreamnite/include_me.git"
+
+metadata
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,55 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+web_root_dir = node['devops']['web']['root_dir']
+web_port     = node['devops']['web']['port']
+# ---------------------------------------------------------------------------
+# We're assuming rhel/centos
+# ---------------------------------------------------------------------------
+return if !node[:platform_family].include?("rhel")
+# ---------------------------------------------------------------------------
+# Install httpd & associated service
+# ---------------------------------------------------------------------------
+package 'Install Apache' do
+    package_name 'httpd'
+end
+service "httpd" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable ]
+end
+# ---------------------------------------------------------------------------
+# Configure httpd
+# ---------------------------------------------------------------------------
+template '/etc/httpd/conf/httpd.conf' do
+  source 'httpd.conf.erb'
+  user 'root'
+  group 'root'
+  mode '644'
+  variables ({
+      'web_port'      => web_port,
+      'web_root_dir'  => web_root_dir
+  })
+  notifies :restart, "service[httpd]"
+end
+directory web_root_dir do
+    user 'root'
+    group 'root'
+    mode '755'
+    notifies :restart, "service[httpd]"
+end
+# ---------------------------------------------------------------------------
+# Ensure correct selinux context
+# ---------------------------------------------------------------------------
+package 'Install semanage' do
+    package_name 'policycoreutils-python'
+end
+# semanage fcontext -a -e /var/www/html /opt/mywebapp
+execute 'web-selinux' do
+    command '/usr/sbin/semanage fcontext -a -e ${webBaseDir} ${webRootDir}'
+    environment(
+      'webBaseDir' => '/var/www/html',
+      'webRootDir' => web_root_dir
+    )
+    not_if '/usr/sbin/semanage export | /bin/grep "${webBaseDir} ${webRootDir}"'
+    notifies :restart, "service[httpd]"
+end
