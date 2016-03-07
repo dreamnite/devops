b33c4d3588036cc1b9a58e589c0bb181261abf86
Mike Morrow <mmorrow@vt.edu>
2016-03-06 22:42:19 -0500

    Unnecessary switch to Ubuntu.

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'Installs/Configures devops'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'include_me'
+issues_url       'https://github.com/MikeMorrow/devops'
+source_url       'https://github.com/MikeMorrow/devops'
+supports         'rhel, centos, debian, ubuntu'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,45 +7,56 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe 'include_me'
-web_root_dir = node['devops']['web']['root_dir']
-web_port     = node['devops']['web']['port']
-test_url     = node['devops']['test_url']
 # ---------------------------------------------------------------------------
-# We're assuming rhel/centos
+# Set variables
 # ---------------------------------------------------------------------------
-return if !node[:platform_family].include?("rhel")
+webRootDir = node['devops']['web']['root_dir']
+webPort    = node['devops']['web']['port']
+test_url   = node['devops']['test_url']
+# platform specific variables
+case node['platform'] # or platform_family
+when 'redhat', 'centos' # or rhel
+    apachePackageName = 'httpd'
+    apacheConfigTemplate = 'httpd.conf.erb'
+    apacheConfigFile = '/etc/httpd/conf/httpd.conf'
+when 'ubuntu', 'debian' # or debian
+    apachePackageName =  'apache2'
+    apacheConfigTemplate = '000-default.conf.erb'
+    apacheConfigFile = '/etc/apache2/sites-available/000-default.conf'
+end
 # ---------------------------------------------------------------------------
 # Install httpd & associated service
 # ---------------------------------------------------------------------------
 package 'httpd' do
-    action :install
+  package_name apachePackageName
+  action :install
 end
 service 'httpd' do
+  service_name apachePackageName
   supports :status => true, :restart => true, :reload => true
-  action [ :enable ]
+  action :enable
 end
 # ---------------------------------------------------------------------------
 # Configure httpd
 # ---------------------------------------------------------------------------
-template '/etc/httpd/conf/httpd.conf' do
-  source 'httpd.conf.erb'
+template apacheConfigFile do
+  source apacheConfigTemplate
   user 'root'
   group 'root'
   mode '644'
   variables ({
-      'web_port'      => web_port,
-      'web_root_dir'  => web_root_dir
+      'webPort'     => webPort,
+      'webRootDir'  => webRootDir
   })
   notifies :restart, "service[httpd]"
 end
-directory web_root_dir do
+directory webRootDir do
     user 'root'
     group 'root'
     mode '755'
     notifies :restart, "service[httpd]"
 end
-
-template "#{web_root_dir}/index.html" do
+template "#{webRootDir}/index.html" do #FoodCritic is confused here
   source 'index.html.erb'
   user 'root'
   group 'root'
@@ -53,7 +64,23 @@ template "#{web_root_dir}/index.html" do
   variables ({
       'test_url' => test_url
   })
+  notifies :restart, 'service[httpd]'
 end
+template '/etc/apache2/ports.conf' do #FoodCritic is confused here
+  source 'ports.conf.erb'
+  user 'root'
+  group 'root'
+  mode '644'
+  variables ({
+      'webPort' => webPort
+  })
+  notifies :restart, "service[httpd]"
+  only_if "node['platform'].include?('ubuntu')"
+end
+# ---------------------------------------------------------------------------
+# Exit before selinux fixes if we're not on rhel/centos
+# ---------------------------------------------------------------------------
+return unless node['platform_family'].include?("rhel")
 # ---------------------------------------------------------------------------
 # Ensure correct selinux context
 # ---------------------------------------------------------------------------
@@ -65,7 +92,7 @@ execute 'web-selinux' do
     command '/usr/sbin/semanage fcontext -a -e ${webBaseDir} ${webRootDir}'
     environment(
       'webBaseDir' => '/var/www/html',
-      'webRootDir' => web_root_dir
+      'webRootDir' => webRootDir
     )
     not_if '/usr/sbin/semanage export | /bin/grep "${webBaseDir} ${webRootDir}"'
     notifies :restart, "service[httpd]"
