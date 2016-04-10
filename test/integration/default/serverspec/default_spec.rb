465e917b0be2f56ccde40b62c83e1b0a0a91d0c3
Madhu Joshi <madhu.joshi@gmail.com>
2016-04-09 21:39:22 -0400

    created ServerSpec recipes

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'devops::default' do
+  # Serverspec examples can be found at
+  # http://serverspec.org/resource_types.html
+  it 'does something' do
+    skip 'Placeholder.'
+  end
+end
