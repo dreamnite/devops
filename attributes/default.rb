31217d8e83515ece80c6efa9f78fdcde403dc2e9
Eduardo Romero <edromero@users.noreply.github.com>
2016-06-08 18:47:59 -0400

    2.2.1 Recipe creation

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default[:devops][:webserver][:port]				=	8080
+default[:devops][:webserver][:document_root]	=	'/opt/mywebapp'
+default[:devops][:test_url]						=	'https://github.com/edromero/devops'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,25 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+document_root	=	node[:devops][:webserver][:document_root]
+port			=	node[:devops][:webserver][:port]
+test_url		=	node[:devops][:test_url]
+
+
+directory document_root
+
+file "#{document_root}/index.html" do
+	content "This website created by Chef for the Devops skill test located at #{test_url}"
+end
+
+package 'httpd'
+
+template '/etc/httpd/conf.d/000_test_url.conf' do
+	source '000_test_url.conf.erb'
+	variables( :document_root => document_root, :port => (port || 8080) )
+end
+
+service 'httpd' do
+	action [:start, :enable]
+end
