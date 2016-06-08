c048477ca5681aeef4393da4e78b138baaf45b2b
Eduardo Romero <edromero@users.noreply.github.com>
2016-06-08 19:04:02 -0400

    Fixes for FC064 and FC065. For FC001, I prefer to use colons because they are still simple to read and quicker to write

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures devops'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends			 'include_me'
+issues_url 'https://github.com/edromero/devops/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/edromero/devops' if respond_to?(:source_url)
