35bce859de9bc1d99f2bba80f2135753cd265392
Eduardo Romero <edromero@users.noreply.github.com>
2016-06-08 11:50:50 -0400

    Completed section 1 of test

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'devops'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Eduardo Romero'
+maintainer_email 'eromero@fake.com'
 license          'CC-BY-SA'
 description      'Installs/Configures devops'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
